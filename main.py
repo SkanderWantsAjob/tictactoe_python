6a777ed6cf713dbc8d277668c044e44ea34c401e
Ben Achour Skander <benachour.skander1@gmail.com>
2024-03-01 15:58:30 +0100

    functionnal tic tac tao with winning and losing

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 
 def printgameboard():
-    for x in range(rows):
+    for x in range(grows):
         print("+---+---+---+---+")
-        for y in range (cols):
+        for y in range (gcols):
             print("|", end="")
             print(" ", gameboard[x][y], end= " |")
         print()
@@ -13,8 +13,8 @@ print("welcome to Tic Tac Toe")
 print("****************************************")
 numbersleft=list(range(1,10))
 gameboard=[[1,2,3],[4,5,6],[7,8,9]]
-rows=3
-cols=3
+grows=3
+gcols=3
 
 
 def modifyboard(num, turn):
@@ -24,6 +24,21 @@ def modifyboard(num, turn):
 leaveloop= False
 turn = 'X'
 turncounter=0
+def someonewon():
+    for row in gameboard:
+        if( all(cell == 'X' for cell in row ) or all (cell == 'O' for cell in row)):
+            return True
+    for x in range(3):
+        if (all(row[x]=='X' for row in gameboard ) or all(row[x]== 'O' for row in gameboard)):
+            return True
+    if all(gameboard[i][i] == 'X' for i in range(3)) or all(gameboard[i][2 - i] == 'X' for i in range(3)):
+        return True
+    if all(gameboard[i][i] == 'O' for i in range(3)) or all(gameboard[i][2 - i] == 'O' for i in range(3)):
+        return True
+
+    return False
+
+
 def playerturn():
     while (True):
         playerchoice = int(input("\n Choose a number from 1 to 9\n"))
@@ -43,7 +58,8 @@ def computerturn():
             modifyboard(cpuchoice, 'O')
             break
 
-while (turncounter < 9):
+gameover= False
+while (gameover == False):
     if(turncounter % 2 == 0):
     #X turn
         printgameboard()
@@ -53,6 +69,9 @@ while (turncounter < 9):
         computerturn()
     print(turncounter)
     turncounter += 1
+    gameover= someonewon()
+if (gameover == True):
+    print("SOMEONE WON I THINK")
 print("FOR THE FINAL OUTPUT")
 printgameboard()
 
