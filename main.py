93b027eeda084b0330f13428259a184b806748cc
Ben Achour Skander <benachour.skander1@gmail.com>
2024-03-01 12:50:52 +0100

    second commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ numbersleft=list(range(1,10))
 gameboard=[[1,2,3],[4,5,6],[7,8,9]]
 rows=3
 cols=3
-printgameboard()
+
 
 def modifyboard(num, turn):
     num-=1
@@ -24,30 +24,33 @@ def modifyboard(num, turn):
 leaveloop= False
 turn = 'X'
 turncounter=0
+def playerturn():
+    while (True):
+        playerchoice = int(input("\n Choose a number from 1 to 9\n"))
+        if (playerchoice > 9 or playerchoice < 1 or playerchoice not in numbersleft):
+            print("invalid, you gotta choose from ", numbersleft)
+            continue
+        numbersleft.remove(playerchoice)
+        modifyboard(playerchoice, 'X')
+        break
+
+def computerturn():
+    while (True):
+        cpuchoice = random.choice(numbersleft)
+        if (cpuchoice in numbersleft):
+            print("THE ALMIGHTY COMPUTER HAS CHOSEN ", cpuchoice)
+            numbersleft.remove(cpuchoice)
+            modifyboard(cpuchoice, 'O')
+            break
+
 while (turncounter < 9):
     if(turncounter % 2 == 0):
-        #X turn
+    #X turn
         printgameboard()
-        while(True):
-            playerchoice = int(input("\n Choose a number [1-9]\n"))
-            if (playerchoice > 9 or playerchoice < 1 or playerchoice not in numbersleft ):
-                print("invalid, you gotta choose from ", numbersleft)
-                continue
-            numbersleft.remove(playerchoice)
-            modifyboard(playerchoice, 'X')
-            break
-
-
-
+        playerturn()
     else:
         #O turn
-        while(True):
-            cpuchoice= random.choice(numbersleft)
-            if (cpuchoice in numbersleft):
-                print("THE ALMIGHTY COMPUTER HAS CHOSEN ", cpuchoice)
-                numbersleft.remove(cpuchoice)
-                modifyboard(cpuchoice, 'O')
-            break
+        computerturn()
     print(turncounter)
     turncounter += 1
 print("FOR THE FINAL OUTPUT")
